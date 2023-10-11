0aacacaf37ee746c5cf56957ad502af534b33dfe
Jserin <jsr3643@naver.com>
2023-10-12 01:14:14 +0900

    8강 완

--- src/main/java/com/ll/rest_api/boundedContext/member/controller/MemberController.java
+++ src/main/java/com/ll/rest_api/boundedContext/member/controller/MemberController.java
@@ -1,15 +1,36 @@
 package com.ll.rest_api.boundedContext.member.controller;
 
+import com.ll.rest_api.boundedContext.member.entity.Member;
+import com.ll.rest_api.boundedContext.member.service.MemberService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController //responsebody 안붙여도댐
-@RequestMapping("/member")
-public class MemberController {
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
-    @PostMapping("/login")
-    public String login() {
-        return "redirect:/";
+@RequiredArgsConstructor
+@RestController //responsebody 안붙여도댐
+@RequestMapping(value = "/member", produces = APPLICATION_JSON_VALUE, consumes = APPLICATION_JSON_VALUE) // json을 생산하고 입력받는것도 json임을 명시
+public class MemberController {
+    private final MemberService memberService;
+
+    @Data
+    public static class LoginRequest {
+        @NotBlank
+        private String username;
+        @NotBlank
+        private String password;
+    }
+
+    @PostMapping("/login")//requestBody가 있어야 json으로 받은 파일 해석할 수 있음
+    public Member login(@Valid @RequestBody LoginRequest loginRequest) {
+        return memberService.findByUsername(loginRequest.getUsername()).orElse(null);
     }
 }
