89bfdcd66cdb5380e32e392b63e819d28ae71635
Jserin <jsr3643@naver.com>
2023-11-29 14:24:45 +0900

    231129 springdoc

--- src/main/java/com/ll/rest_api/boundedContext/member/controller/ApiV1MemberController.java
+++ src/main/java/com/ll/rest_api/boundedContext/member/controller/ApiV1MemberController.java
@@ -3,6 +3,8 @@ package com.ll.rest_api.boundedContext.member.controller;
 import com.ll.rest_api.base.rsData.RsData;
 import com.ll.rest_api.boundedContext.member.entity.Member;
 import com.ll.rest_api.boundedContext.member.service.MemberService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
@@ -20,6 +22,7 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 @RequiredArgsConstructor
 @RestController //responsebody 안붙여도댐
 @RequestMapping(value = "/api/v1/member", produces = APPLICATION_JSON_VALUE, consumes = APPLICATION_JSON_VALUE) // json을 생산하고 입력받는것도 json임을 명시
+@Tag(name = "ApiV1MemberController", description = "로그인, 로그인 된 회원의 정보")
 public class ApiV1MemberController {
     private final MemberService memberService;
 
@@ -38,6 +41,7 @@ public class ApiV1MemberController {
     }
 
     @PostMapping("/login")//requestBody가 있어야 json으로 받은 파일 해석할 수 있음
+    @Operation(summary = "로그인, 엑세스 토큰 발급")
     public RsData<LoginResponse> login(@Valid @RequestBody LoginRequest loginRequest) {
         String accessToken = memberService.genAccessToken(loginRequest.getUsername(), loginRequest.getPassword());
 
@@ -52,6 +56,7 @@ public class ApiV1MemberController {
 
     // consumes = ALL_VALUE => 나는 딱히 JSON 을 입력받기를 고집하지 않겠다.
     @GetMapping(value = "/me", consumes = ALL_VALUE)
+    @Operation(summary = "로그인된 사용자의 정보")
     public RsData<MeResponse> me(@AuthenticationPrincipal User user) {
         Member member = memberService.findByUsername(user.getUsername()).get();
 
